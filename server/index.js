f6c33ed9d80d3aaffca49e1abbb04b44070143a9
epifaniagb <epifaniaegb@gmail.com>
2024-04-22 17:43:23 -0400

    code finished?

--- server/index.js
+++ server/index.js
@@ -1,8 +1,27 @@
 const express = require('express');
 const app = express();
 
-app.get('/header', (req, res) => {
-  res.send('<h1>I am a header!</h1>');
+app.get('/home', (req, res, next) => {
+  res.send('<h1>Welcome home!</h1>');
+});
+
+app.get('/about', (req, res, next) => {
+  res.send('<p>This is the about page</p>');
+});
+
+app.get('/api/data', (req, res, next) => {
+  const data = {
+    message: 'This is raw data from the server.',
+  };
+  res.send(data);
+});
+
+app.get('/api/filter', (req, res, next) => {
+  const filter = req.query.filter;
+  const filteredData = {
+    message: `This data is filtered by ${filter}.`,
+  };
+  res.send(filteredData);
 });
 
 const port = 8080;
